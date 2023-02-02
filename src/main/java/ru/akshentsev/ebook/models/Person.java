1e702f5c3f39bf8383d79c101e4afe2ff7930714
Konstantin Akshentsev <akshentsev69@gmail.com>
2023-02-02 08:21:38 +0300

    It's a simple Java CRUD app with Spring and PostgreSQL. For connect to DB I use JDBCTemplate

--- src/main/java/ru/akshentsev/ebook/models/Person.java
+++ src/main/java/ru/akshentsev/ebook/models/Person.java
@@ -1,2 +1,41 @@
-package ru.akshentsev.ebook.models;public class Person {
+package ru.akshentsev.ebook.models;
+
+import javax.validation.constraints.*;
+
+public class Person {
+    private int id;
+    @NotEmpty(message = "Имя не должно быть пустым")
+    @Size(min = 2, max = 100, message = "Имя должно быть от 2 до 100 символов длиной")
+    private String fullName;
+    @Min(value = 1900, message = "Год рождения должен быть больше, чем 1900")
+    private int yearOfBirth;
+    public Person(){}
+    public int getId() {
+        return id;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Person(String fullName, int yearOfBirth) {
+        this.fullName = fullName;
+        this.yearOfBirth = yearOfBirth;
+    }
 }
--- src/main/java/ru/akshentsev/ebook/util/PersonValidator.java
+++ src/main/java/ru/akshentsev/ebook/util/PersonValidator.java
@@ -1,2 +1,31 @@
-package ru.akshentsev.ebook.util;public class PersonValidator {
+package ru.akshentsev.ebook.util;
+
+import ru.akshentsev.ebook.dao.PersonDAO;
+import ru.akshentsev.ebook.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class PersonValidator implements Validator {
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PersonValidator(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return Person.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Person person = (Person) o;
+        // Посмотреть есть ли человек с таким же email в БД
+        if (personDAO.getPersonByFullName(person.getFullName()).isPresent())
+            errors.rejectValue("fullName", "", "Человек к таким именем уже существует");
+    }
 }
